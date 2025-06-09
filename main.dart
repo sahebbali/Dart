dd81f7f8acdaee28d0fb8c84a4c707328e8b7252
sahebbali <saheb1996jmr@gmail.com>
2025-06-09 18:38:51 +0600

    inital commit

---
+++ main.dart
@@ -0,0 +1,23 @@
+// Define a class
+class Person {
+  String name;
+  int age;
+
+  // Constructor
+  Person(this.name, this.age);
+
+  // Method to display information
+  void displayInfo() {
+    print('Name: $name');
+    print('Age: $age');
+  }
+}
+
+// Main function - entry point of the app
+void main() {
+  // Create a Person object
+  Person person1 = Person('Saheb', 25);
+
+  // Call method to display info
+  person1.displayInfo();
+}
