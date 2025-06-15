98eaa2f902bbd97d10483ea5cc4847d296ca8095
sahebbali <saheb1996jmr@gmail.com>
2025-06-15 20:06:19 +0600

    add final and const

--- oop.dart
+++ oop.dart
@@ -1,26 +1,38 @@
-class Person {
-  String name;
-  int age;
+// class Person {
+//   String name;
+//   int age;
 
-  // default constructor with initializer list
-  Person(this.name, [this.age = 18]);
+//   // default constructor with initializer list
+//   Person(this.name, [this.age = 18]);
 
-  // named constructor
-  Person.guest() : name = 'Guest', age = 18;
+//   // named constructor
+//   Person.guest() : name = 'Guest', age = 18;
 
-  void showOutput() {
-    print('Name: $name');
-    print('Age: $age');
-  }
-}
+//   void showOutput() {
+//     print('Name: $name');
+//     print('Age: $age');
+//   }
+// }
 
+// void main() {
+//   var person1 = Person('Jack');
+//   Person person2 = Person('Jill', 15);
+
+//   person1.showOutput();
+//   person2.showOutput();
+
+//   var person3 = Person.guest();
+//   person3.showOutput();
+// }
 void main() {
-  var person1 = Person('Jack');
-  Person person2 = Person('Jill', 15);
+  final String person1 = 'Jack';
+  const String person2 = 'Jill'; // compile time constant
 
-  person1.showOutput();
-  person2.showOutput();
-
-  var person3 = Person.guest();
-  person3.showOutput();
+  print(person1);
+  print(person2);
 }
+
+
+// can not reassign
+// person1 = 'aa';
+// person2 = 'bb';
