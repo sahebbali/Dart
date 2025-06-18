127d179357929e1175dc306740033b584edb159f
sahebbali <saheb1996jmr@gmail.com>
2025-06-18 10:04:19 +0600

    add file write

--- file.dart
+++ file.dart
@@ -1,9 +1,19 @@
 // File Reading
 
+// import 'dart:io';
+
+// void main() async {
+//   File file = File('data.txt');
+//   var contents = await file.readAsString();
+//   print(contents);
+// }
+
 import 'dart:io';
 
 void main() async {
   File file = File('data.txt');
+  var content = await file.writeAsString('Life is Good!\n\nI love programming');
+  print('File written successfully: $content');
   var contents = await file.readAsString();
-  print(contents);
+  print('File contents: $contents');
 }
