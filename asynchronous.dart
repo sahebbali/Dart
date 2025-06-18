3c922141adecd300e32642755b6d622713f6c670
sahebbali <saheb1996jmr@gmail.com>
2025-06-18 09:21:01 +0600

    add asyncronous

---
+++ asynchronous.dart
@@ -0,0 +1,14 @@
+Future delayedPrint(int seconds, String msg) {
+  final duration = Duration(seconds: seconds);
+  return Future.delayed(duration).then((value) => msg);
+}
+
+main() {
+  print('Life');
+  delayedPrint(2, "Is")
+      .then((status) {
+        print(status);
+      })
+      .catchError((err) => print(err));
+  print('Good');
+}
